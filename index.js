131774e561bac04241b594b756126d8dc8d28977
Chiou <Sheng.Chiou@aceraeb.com>
2020-10-05 17:10:18 +0800

    update

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ let linebot = require('linebot');
 const fetch = require('node-fetch')
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
-// let bot = linebot({
-//     channelId: process.env.LINE_CH_ID,
-//     channelSecret: process.env.LINE_CH_SECRET,
-//     channelAccessToken: process.env.LINE_ACCESS_TOKEN
-// });
-
-console.log('檢測變數', process.env.LINE_CH_ID, process.env.LINE_CH_SECRET, process.env.LINE_ACCESS_TOKEN)
-
 let bot = linebot({
-    channelId: '1655016363',
-    channelSecret: '3daaba3801a47b66f9aea349af7999e8',
-    channelAccessToken: 'DO8nAW3G4XSbMzmzflnh68fY1VVIcNLMtJ24nl1XYq28PFnTYV6tPp1qS8vrn9CECSI9034it8tc3gXQI4Upn1Zx/qwwAfCqmwpBcuK0KZtx8n1g5mU/rRhbwAXCuEVg8wDHUVMY/R+GqXBgU9XtugdB04t89/1O/w1cDnyilFU='
-
+    channelId: process.env.LINE_CH_ID,
+    channelSecret: process.env.LINE_CH_SECRET,
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN
 });
+
+// console.log('檢測變數', process.env.LINE_CH_ID, process.env.LINE_CH_SECRET, process.env.LINE_ACCESS_TOKEN)
+
+// let bot = linebot({
+//     channelId: '1655016363',
+//     channelSecret: '3daaba3801a47b66f9aea349af7999e8',
+//     channelAccessToken: 'DO8nAW3G4XSbMzmzflnh68fY1VVIcNLMtJ24nl1XYq28PFnTYV6tPp1qS8vrn9CECSI9034it8tc3gXQI4Upn1Zx/qwwAfCqmwpBcuK0KZtx8n1g5mU/rRhbwAXCuEVg8wDHUVMY/R+GqXBgU9XtugdB04t89/1O/w1cDnyilFU='
+
+// });
 
 
 const token = 'DO8nAW3G4XSbMzmzflnh68fY1VVIcNLMtJ24nl1XYq28PFnTYV6tPp1qS8vrn9CECSI9034it8tc3gXQI4Upn1Zx/qwwAfCqmwpBcuK0KZtx8n1g5mU/rRhbwAXCuEVg8wDHUVMY/R+GqXBgU9XtugdB04t89/1O/w1cDnyilFU='
