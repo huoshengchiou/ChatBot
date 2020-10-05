c061a50a4f0939cb5695db189b0c8bd294c5d84f
Chiou <Sheng.Chiou@aceraeb.com>
2020-10-05 10:44:19 +0800

    update

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ let bot = linebot({
     channelAccessToken: process.env.LINE_U_ID
 });
 
-// 當有人傳送訊息給 Bot 時
+// 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
-    // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
-    event.reply(`你說了 ${event.message.text}`);
-    console.log('hello')
+    // event.message.text是使用者傳給bot的訊息
+    // 準備要回傳的內容
+    var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+    // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+    event.reply(replyMsg).then(function (data) {
+        // 當訊息成功回傳後的處理
+    }).catch(function (error) {
+        // 當訊息回傳失敗後的處理
+    });
 });
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
